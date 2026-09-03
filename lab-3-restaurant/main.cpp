5ebfe1a9c3d804d6ddf0c5258f0381d305d5cdc6
Andrewwuuu-9527 <andrewwuuu512874@gmail.com>
2026-09-03 20:05:20 +0800

    feat: implement populateRestaurant function, Milestone 1 complete

--- lab-3-restaurant/main.cpp
+++ lab-3-restaurant/main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <string>
 #include <iomanip>
+#include <cctype>
 using namespace std;
 
 struct Restaurant{
@@ -17,5 +18,45 @@ void displayRestaurant(const Restaurant &r);
 
 int main(){
 
+    Restaurant testRestaurant = populateRestaurant();
+
+    cout << "\n--- Test: Single Restaurant Filled ---\n";
+    cout << "Name: " << testRestaurant.name << endl;
+    cout << "Address: " << testRestaurant.address << endl;
+    cout << "Rating: " << testRestaurant.rating << endl;
+    cout << "Price: " << testRestaurant.avgPrice << endl;
+    cout << "Has Delivery: " << (testRestaurant.hasDelivery ? "Yes" : "No") << endl;
+
     return 0;
+}
+
+Restaurant populateRestaurant(){
+    Restaurant temp;
+
+    cout << "Enter restaurant name: ";
+    getline(cin, temp.name);
+
+    cout << "Enter restaurant address: ";
+    getline(cin, temp.address);
+
+    cout << "Enter restaurant rating (1-5): ";
+    cin >> temp.rating;
+    cin.ignore(); 
+    while(temp.rating < 1 || temp.rating > 5){
+        cout << "Invalid. Please enter a rating between 1 and 5: ";
+        cin >> temp.rating;
+    }
+
+    cout << "Enter average price: $";
+    cin >> temp.avgPrice;
+    cin.ignore();
+
+    char deliveryInput;
+    cout << "Has delivery service? (Y/N): ";
+    cin >> deliveryInput;
+    temp.hasDelivery = (toupper(deliveryInput) == 'Y');
+
+    cin.ignore();
+    
+    return temp;
 }
