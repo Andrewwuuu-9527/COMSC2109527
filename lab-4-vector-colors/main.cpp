b142e08a3f4b2dd607f4b80ad3b3e09edd1c3855
Andrewwuuu-9527 <andrewwuuu512874@gmail.com>
2026-09-05 20:32:13 +0800

    feat: define Color struct and test single object

--- lab-4-vector-colors/main.cpp
+++ lab-4-vector-colors/main.cpp
@@ -6,7 +6,18 @@
 #include <iomanip>
 using namespace std;
 
+struct Color {
+    int red;
+    int green;
+    int blue;
+};
+
 int main() {
+    
+    Color testColor = {100, 150, 200};
+    cout << "Test Color - R: " << testColor.red
+         << " G: " << testColor.green
+         << " B: " << testColor.blue << endl;
 
     return 0;
 }
