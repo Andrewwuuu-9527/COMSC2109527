4f6d97466d7421df91b26ad2c2b30fd9e2759e27
Andrewwuuu-9527 <andrewwuuu512874@gmail.com>
2026-09-05 20:27:09 +0800

    feat: initial skeleton for Lab 4

---
+++ lab-4-vector-colors/main.cpp
@@ -0,0 +1,12 @@
+// COMSC-210 | Lab 4 | Andrew
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <iomanip>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
