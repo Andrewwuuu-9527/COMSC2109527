c80a1227c1041e65e38ab7bce251c6f666d0d2a4
Andrewwuuu-9527 <andrewwuuu512874@gmail.com>
2026-09-05 20:40:23 +0800

    feat: add vector<Color> and push one color

--- lab-4-vector-colors/main.cpp
+++ lab-4-vector-colors/main.cpp
@@ -13,11 +13,17 @@ struct Color {
 };
 
 int main() {
-    
-    Color testColor = {100, 150, 200};
-    cout << "Test Color - R: " << testColor.red
-         << " G: " << testColor.green
-         << " B: " << testColor.blue << endl;
+
+    vector<Color> colors;
+    Color temp = {200, 100, 50};
+    colors.push_back(temp);
+
+    cout << "Vector contains " << colors.size() << " color(s)." << endl;
+    for (size_t i = 0; i < colors.size(); ++i) {
+        cout << "Color " << i + 1 << ": R=" << colors[i].red
+             << " G=" << colors[i].green
+             << " B=" << colors[i].blue << endl;
+    }
 
     return 0;
 }
