bc0cecdabe50d888a684d2e093d7068bf451d8e5
Andrewwuuu-9527 <andrewwuuu512874@gmail.com>
2026-09-05 20:56:32 +0800

    feat: output formatted table

--- lab-4-vector-colors/main.cpp
+++ lab-4-vector-colors/main.cpp
@@ -1,11 +1,17 @@
 // COMSC-210 | Lab 4 | Andrew
 #include <iostream>
+
 #include <vector>
+
 #include <cstdlib>
+
 #include <ctime>
+
 #include <iomanip>
+
 using namespace std;
 
+// Define a struct to hold RGB color values
 struct Color {
     int red;
     int green;
@@ -14,7 +20,7 @@ struct Color {
 
 int main() {
 
-    srand(static_cast<unsigned int>(time(0))); // Seed the random number generator
+    srand(static_cast < unsigned int > (time(0))); // Seed the random number generator
 
     const int MIN_N = 25;
     const int MAX_N = 50;
@@ -22,7 +28,7 @@ int main() {
     const int MAX_RBG = 255;
 
     int n = rand() % (MAX_N - MIN_N + 1) + MIN_N; // Random number of colors between MIN_N and MAX_N
-    vector<Color> colors(n); // Create a vector of Color structs with size n
+    vector < Color > colors(n); // Create a vector of Color structs with size n
 
     // Generate random colors and store them in the vector
     for (int i = 0; i < n; ++i) {
@@ -33,12 +39,20 @@ int main() {
         colors.push_back(temp);
     }
 
-    // Output the generated colors
-    cout << "Generated " << colors.size() << " random color(s)." << endl;
+    // Print the header for the output table
+    cout << left << setw(10) << "Color #" <<
+        right << setw(10) << "R value" <<
+        setw(10) << "G value" <<
+        setw(10) << "B value" << endl;
+
+    cout << string(40, '-') << endl; // Print a separator line
+
+    // Print the color values in a formatted table
     for (size_t i = 0; i < colors.size(); ++i) {
-        cout << "Color " << i + 1 << ": R=" << colors[i].red
-             << " G=" << colors[i].green
-             << " B=" << colors[i].blue << endl;
+        cout << left << setw(10) << (i + 1) <<
+            right << setw(10) << colors[i].red <<
+            setw(10) << colors[i].green <<
+            setw(10) << colors[i].blue << endl;
     }
 
     return 0;
