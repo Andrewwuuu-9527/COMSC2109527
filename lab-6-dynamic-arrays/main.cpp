978bb05c80c0160a1cb1b6718fe884c373b3ad0a
Andrewwuuu-9527 <andrewwuuu512874@gmail.com>
2026-09-12 21:36:21 +0800

    feat: add function prototypes

--- lab-6-dynamic-arrays/main.cpp
+++ lab-6-dynamic-arrays/main.cpp
@@ -2,7 +2,12 @@
 #include <iostream>
 using namespace std;
 
-const int SIZE = 5;
+const int SIZE = 5; // Size of the dynamic array
+
+// Function prototypes
+void enterArrayData(double *arr, int size);
+void outputArrayData(double *arr, int size);
+void sumArry(double *arr, int size);
 
 int main() {
     double *arr = new double[SIZE];
