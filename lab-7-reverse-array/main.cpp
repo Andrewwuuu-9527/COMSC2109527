012743ed384d7ead36b3878aa2ee58165bf57c9c
Andrewwuuu-9527 <andrewwuuu512874@gmail.com>
2026-09-13 18:19:19 +0800

    style: final cleanup and pointer notation check

--- lab-7-reverse-array/main.cpp
+++ lab-7-reverse-array/main.cpp
@@ -1,16 +1,18 @@
 // COMSC-210 | Lab 7 | Andrew
 #include <iostream>
+
 #include <string>
+
 using namespace std;
 
 const int SIZE = 5; // Size of the array    
 
 // Function prototypes
-string* reverseArray(string *arr, int size);
-void displayArray(string *arr, int size);
+string * reverseArray(string * arr, int size);
+void displayArray(string * arr, int size);
 
 int main() {
-    string *arr = new string[SIZE];
+    string * arr = new string[SIZE];
 
     // Initialize the array with some values
     *(arr + 0) = "Janet";
@@ -35,26 +37,26 @@ int main() {
 }
 
 // Function to reverse the array
-void displayArray(string *arr, int size) {
+void displayArray(string * arr, int size) {
     for (int i = 0; i < size; ++i) {
-        cout << *(arr + i) << " ";
+        cout << * (arr + i) << " ";
     }
     cout << endl;
 }
 
 // Function to reverse the array
-string* reverseArray(string *arr, int size) {
+string * reverseArray(string * arr, int size) {
     int left = 0;
     int right = size - 1;
 
     // Swap elements from both ends of the array until the middle is reached
     while (left < right) {
-        string temp = *(arr + left);
-        *(arr + left) = *(arr + right);
+        string temp = * (arr + left);
+        *(arr + left) = * (arr + right);
         *(arr + right) = temp;
 
         left++;
         right--;
-    } 
+    }
     return arr;
 }
