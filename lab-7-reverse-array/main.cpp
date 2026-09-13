7853ea238e43bd066cc5037c1f9dab65a9f02a38
Andrewwuuu-9527 <andrewwuuu512874@gmail.com>
2026-09-13 18:10:06 +0800

    feat: add function prototypes

--- lab-7-reverse-array/main.cpp
+++ lab-7-reverse-array/main.cpp
@@ -3,7 +3,11 @@
 #include <string>
 using namespace std;
 
-const int SIZE = 5;
+const int SIZE = 5; // Size of the array    
+
+// Function prototypes
+string* reverseArray(string *arr, int size);
+void displayArray(string *arr, int size);
 
 int main() {
     string *arr = new string[SIZE];
