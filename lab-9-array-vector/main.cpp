002223cd917e6a453dfed2e63a9db094a340a1a4
Andrewwuuu-9527 <andrewwuuu512874@gmail.com>
2026-09-18 17:36:37 +0800

    feat: demonstrate std::array member functions (size, front, back, at, data, empty)

--- lab-9-array-vector/main.cpp
+++ lab-9-array-vector/main.cpp
@@ -25,7 +25,28 @@ int main() {
     }
     fin.close();
 
-    cout << "Read " << count << " temperatures from file.\n";
+    cout << "Read " << count << " temperatures.\n\n";
+
+    // Display array size
+    cout << "1. Array size: " << temps.size() << endl;
+
+    // Display first and last temperatures
+    cout << "2. First temp: " << temps.front() << endl;
+    cout << "   Last temp: " << temps.back() << endl;
+
+    // Display temperature at index 5
+    cout << "3. Temp at index 5: " << temps.at(5) << endl;
+
+    // Display raw array address
+    cout << "4. Raw array address: " << temps.data() << endl;
+
+    // Check if the array is empty
+    cout << "5. Is empty: " << (temps.empty() ? "Yes" : "No") << endl;
+
+    // Display all temperatures
+    cout << "6. All temperatures: ";
+    for (double t : temps) cout << t << " ";
+    cout << endl << endl;
 
     return 0;
 }
