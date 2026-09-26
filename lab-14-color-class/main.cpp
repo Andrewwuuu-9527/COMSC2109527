9b0250318b1e5ae41b3275712b6e6c5dd5a1a609
Andrewwuuu-9527 <andrewwuuu512874@gmail.com>
2026-09-26 22:20:21 +0800

    feat: add Color class skeleton with private RGB members

---
+++ lab-14-color-class/main.cpp
@@ -0,0 +1,21 @@
+// COMSC-210 | Lab 14 | Andrew
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+// Color class, represents an RGB color with three private integer members
+class Color {
+private:
+    int red;
+    int green;
+    int blue;
+
+public:
+    // Constructor: initialize all channels to 0
+    Color() : red(0), green(0), blue(0) {}
+};
+
+int main() {
+
+    return 0;
+}
